add0ff3ab548b5b861bd48e46ed5c61e82879a1e
Bogdan5 <bogdan5x@gmail.com>
2018-02-11 21:18:49 -0500

    'Comment'

--- src/components/Marked.js
+++ src/components/Marked.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+//
+//
+// class Marked extends Component {
+//   var marked = require('marked');
+//   marked.setOptions({
+//     renderer: new marked.Renderer(),
+//     gfm: true,
+//     tables: false,
+//     breaks: false,
+//     pedantic: false,
+//     sanitize: false,
+//     smartLists: false,
+//     smartypants: false,
+//     xhtml: false
+//   });
+//   let cont = marked (this.props.content);
+//   render() {
+//     return (
+//       <div className="Marked">
+//         <div>Div 2 {cont}</div>
+//       </div>
+//     );
+//   }
+// }
 
-class Marked extends Component {
-  var marked = require('marked');
-  marked.setOptions({
-    renderer: new marked.Renderer(),
-    gfm: true,
-    tables: false,
-    breaks: false,
-    pedantic: false,
-    sanitize: false,
-    smartLists: false,
-    smartypants: false,
-    xhtml: false
-  });
-  let cont = marked (this.props.content);
+import React from 'react';
+import marked from 'marked';
+
+class Marked extends React.Component {
+  getMarkdownText() {
+    console.log("marked cont");
+    var rawMarkup = marked(this.props.content, {sanitize: true});
+    return { __html: rawMarkup };
+  }
   render() {
-    return (
-      <div className="Marked">
-        <div>Div 2 {cont}</div>
-      </div>
-    );
+    console.log("marked starting");
+    return <div dangerouslySetInnerHTML={this.getMarkdownText()} />
   }
 }
 
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
 
-const TextArea = (props) => (
-  <div className="form-group">
-    <label className="form-label">{props.title}</label>
-    <textarea
-      className="form-input"
-      style={props.resize ? null : {resize: 'none'}}
-      name={props.name}
-      rows={props.rows}
-      value={props.content}
-      onChange={props.controlFunc}
-      placeholder={props.placeholder} />
-  </div>
-);
 
-TextArea.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  rows: React.PropTypes.number.isRequired,
-  name: React.PropTypes.string.isRequired,
-  content: React.PropTypes.string.isRequired,
-  resize: React.PropTypes.bool,
-  placeholder: React.PropTypes.string,
-  controlFunc: React.PropTypes.func.isRequired
-};
+class TextArea extends Component {
+  controlFunc = (e) => {
+    console.log("TextArea"+e.target.value)
+    this.newVar=e.target.value;
+  }
+
+  getCont = () =>this.props.callbackpar(this.newVar);
+
+  render(){
+     return <textarea
+      className="form-input"
+      // style={props.resize ? null : {resize: 'none'}}
+      // name={props.name}
+      // rows={props.rows}
+      // value={props.content}
+      onChange={this.controlFunc}
+      // callbackpar={this.getCont}
+       />
+  }
+}
+// const TextArea = (props) => (
+//   <div className="form-group">
+//     <label className="form-label">{props.title}</label>
+//     <textarea
+//       className="form-input"
+//       style={props.resize ? null : {resize: 'none'}}
+//       name={props.name}
+//       rows={props.rows}
+//       value={props.content}
+//       onChange={props.controlFunc}
+//       placeholder={props.placeholder} />
+//   </div>
+// );
+
+// TextArea.propTypes = {
+//   title: React.PropTypes.string.isRequired,
+//   rows: React.PropTypes.number.isRequired,
+//   name: React.PropTypes.string.isRequired,
+//   content: React.PropTypes.string.isRequired,
+//   resize: React.PropTypes.bool,
+//   placeholder: React.PropTypes.string,
+//   controlFunc: React.PropTypes.func.isRequired
+// };
 
 export default TextArea
